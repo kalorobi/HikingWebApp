04730ab6d846f1ffc2eab9f7941e931a143dd840
kalorobi <kalo.robi@gmail.com>
2026-05-05 23:23:45 +0200

    feature: base.css letrehozva

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import Live from './pages/Live';
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
       <Routes>
         <Route path="/live" element={<Live />} />
         <Route path="/live/:user" element={<Live />} />
-        <Route path="/" element={<h1>Hamarosan</h1>} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
