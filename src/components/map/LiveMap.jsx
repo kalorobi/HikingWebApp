a754d653f56b83d4efd0587e64f011b7b3be92a9
kalorobi <kalo.robi@gmail.com>
2026-05-20 22:51:12 +0200

    feature: aktualis poziciora ugras gomb Closed #39

--- src/components/map/LiveMap.jsx
+++ src/components/map/LiveMap.jsx
@@ -1,17 +1,20 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import './LiveMap.css';
-import styles from './LayerStyle.json'
-import mokus from '../../assets/ikons/mokus.svg'
+import styles from './LayerStyle.json';
+import mokus from '../../assets/ikons/mokus.svg';
+import { LocateFixed, Locate } from './MapControls';
 
 export default function LiveMap ({geojson, planGeojson}) {
 
+  const [is_center, set_is_center] = useState(true);
   const mapContainer = useRef(null);
   const map = useRef(null);
   const marker = useRef(null);
+  const icon = useRef(null);
   const popup = useRef(null);
-  const agasvar = [19.826587,47.9263058];
+  const center = useRef(...styles.startPozition.center);
 
   // geojson pontonkent erkezik, mert
   // benne vannak az informaciok gsm, battery, idopontok,
@@ -50,9 +53,19 @@ export default function LiveMap ({geojson, planGeojson}) {
       map.current.addLayer({ source: 'route', ...styles.route });
     });
 
+    // Map elmozdulas figyeles ikoncsere miatt
+    map.current.on('moveend', (e) => {
+      if (e.originalEvent) {
+        set_is_center(false);
+      }
+    });
+
+    //icon inic
     const el = document.createElement('img');
     el.src = mokus; el.style.width = '60px';el.style.height = '51px';
     el.style.cursor = 'pointer';
+    icon.current = el;
+
     marker.current = new maplibregl.Marker({ element: el })
     .setLngLat([0,0])
     .addTo(map.current);
@@ -71,9 +84,10 @@ export default function LiveMap ({geojson, planGeojson}) {
       //meg nincs turapont
       if (geojson.features.length === 0) {
         popup.current = new maplibregl.Popup({closeButton: true,closeOnClick: false})
-        .setLngLat(agasvar)
+        .setLngLat(styles.startPozition.center)
         .setHTML(`<div>Túra még nem indult el.</div>`).addTo(map.current);
         map.current.flyTo({ ...styles.startPozition, speed: 0.8 });
+        set_is_center(true);
       }
       //tura mar elindult
       else {
@@ -89,7 +103,8 @@ export default function LiveMap ({geojson, planGeojson}) {
         const last = geojson.features[geojson.features.length - 1];
         const [lng, lat] = last.geometry.coordinates;
         marker.current?.setLngLat([parseFloat(lng), parseFloat(lat)]);
-        map.current.flyTo({ center: [parseFloat(lng), parseFloat(lat)], zoom: 14, speed: 0.5 });
+        map.current.flyTo({ center: [parseFloat(lng), parseFloat(lat)], zoom: 14, speed: 0.8 });
+        set_is_center(true);
       }
     };
 
@@ -120,7 +135,31 @@ export default function LiveMap ({geojson, planGeojson}) {
     } else {
       map.current.once('load', updatePlan);
     }
-  }, [planGeojson])
+  }, [planGeojson]);
 
-  return <div ref={mapContainer} style={{ width: '100%', height: '100%' }} />;
+  // Gombnyomasra az aktualis poziciora repul
+  function setCenter(){
+    let lat = 0; let lng = 0;
+    if(geojson.features.length != 0){
+      [lng, lat] = geojson.features[geojson.features.length-1].coordinates;
+    }
+    else {
+      [lng, lat] = (styles.startPozition.center);
+    }
+    map.current.flyTo({ center: [parseFloat(lng), parseFloat(lat)], zoom: 15, speed: 0.5 });
+    set_is_center(true);
+  }
+
+  return (
+    <div style={{ position: 'relative', width: '100%', height: '100%' }}>
+      <div ref={mapContainer} style={{ width: '100%', height: '100%' }} />
+
+      <div style={{position: 'absolute',top: 10,right: 10,display: 'flex',flexDirection: 'column',gap: 8}}>
+        <button style={{ padding: 0, lineHeight: 0 }} onClick={setCenter}>
+          {is_center ? <LocateFixed /> : <Locate />}
+        </button>
+      </div>
+
+    </div>
+  );
 }
